4c9c32dd425961340fa50906d324c89878c75bc4
weiscracker <kyle.weiskircher@gmail.com>
2022-07-21 20:56:09 -0500

    replaced favicon and fixed grammar

--- src/AboutTab.js
+++ src/AboutTab.js
@@ -8,7 +8,7 @@ export const AboutTab = props => {
       <Text>
         SafelySend slows down the process of transferring eth. Use the Send tab
         to send eth to any wallet while providing the other party time to hop on
-        SafelySend, go to the Withdraw tab and verify that the transaction was
+        SafelySend, go to the Withdraw tab, and verify that the transaction was
         sent correctly. During this time, no one else can withdraw the eth. If
         the transaction was sent to the wrong address, just head to the Abort
         tab and revert the transaction.
@@ -16,7 +16,7 @@ export const AboutTab = props => {
       <br></br>
       <Text>
         Additionally, you have the option to send a tip to the contract while
-        sending a transaction (minimal extra gas) which will support future
+        sending a transaction (minimal extra gas), which will support future
         feature upgrades including email notifications, ens lookup, ERC20, and
         more.
       </Text>
